988e98c785a6d7dc076225956f6be49881405ee9
KidArachnid3 <tasconjohann@gmail.com>
2025-03-05 16:13:11 -0500

    Added enemies from RPG to dungeon started battle background

---
+++ GRAD262-SP25 Team Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,73 @@
+﻿using Cainos.PixelArtTopDown_Basic;
+using System.Collections;
+using UnityEngine;
+
+namespace SCCC
+{
+    public class Enemy : Character
+    {
+        public int damageStrength;
+
+        Coroutine damageCoroutine;
+
+        float hitPoints;
+
+        private void OnEnable()
+        {
+            ResetCharacter();
+        }
+
+        public override void ResetCharacter()
+        {
+            hitPoints = startingHitPoints;
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+
+        public override IEnumerator DamageCharacter(int damage, float interval)
+        {
+            while (true)
+            {
+                StartCoroutine(FlickerCharacter());
+
+                hitPoints = hitPoints - damage;
+
+                if (hitPoints <= float.Epsilon)
+                {
+                    KillCharacter();
+                    break;
+                }
+
+                if (interval > float.Epsilon)
+                {
+                    yield return new WaitForSeconds(interval);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            // Because only enemies can only hurt the player
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                TopDownCharacterController player = collision.gameObject.GetComponent<TopDownCharacterController>();
+                player.DamageCharacter(damageStrength);
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (damageCoroutine != null)
+                {
+                    StopCoroutine(damageCoroutine);
+                    damageCoroutine = null;
+                }
+            }
+        }
+    }
+}
